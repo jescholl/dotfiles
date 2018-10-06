507efb20f85cc8bba6a076e30263de1b2c8381d1
Jason Scholl <jason.e.scholl@gmail.com>
2018-10-06 13:55:44 -0700

    add some chef tooling

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,4 @@ alias reload!='. ~/.zshrc'
 alias be='bundle exec'
 alias dc="docker-compose"
 alias yaml2json="ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))'"
+alias knife="be knife"
