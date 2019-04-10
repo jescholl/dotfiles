1901274b32ca1462d651b11ea4740b5fa996f4db
Jason Scholl <jason.scholl@gemini.com>
2019-04-10 14:49:04 -0700

    add python/ansible aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,10 @@
 alias reload!='. ~/.zshrc'
 
+alias prun='pipenv run'
 alias be='bundle exec'
 alias dc="docker-compose"
 alias yaml2json="ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))'"
 alias knife="be knife"
 alias wx="curl wttr.in/Portland"
+
+alias ansible="prun ansible"
