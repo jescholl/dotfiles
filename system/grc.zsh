8525eb3b2367c711675919e5be28a84a55ce0782
Jason Scholl <jason.scholl@gemini.com>
2019-05-31 16:20:04 -0700

    fix grc in linux

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,8 @@
 # GRC colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] ))
-then
-  source `brew --prefix`/etc/grc.bashrc
+if (( $+commands[grc] )); then
+  if  (( $+commands[brew] )); then
+    source `brew --prefix`/etc/grc.bashrc
+  else
+    source /etc/grc.zsh
+  fi
 fi
