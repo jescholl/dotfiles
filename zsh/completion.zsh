68cbe203205f2bc6f4ce5a85985c98a6dcc64fef
Jason Scholl <jason.e.scholl@gmail.com>
2019-12-30 08:41:35 -0800

    optimize completion functions and fpath

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,15 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# completion menu
+zstyle ':completion:*' menu select
+
+
+typeset -u completion_files
+completion_files=($DOTFILES_ROOT/**/functions/_*)
+
+for func in $completion_files; do
+  compdef "${func##*/}" "${func##*/_}"
+done
+unset completion_files
