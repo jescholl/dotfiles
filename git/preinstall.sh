40804ffc37780a7446c042baf7da30fcb64dd109
Jason Scholl <jason.e.scholl@gmail.com>
2020-01-04 14:42:51 -0800

    WIP

--- git/preinstall.sh
+++ git/preinstall.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
+
+source "$DOTFILES_ROOT/bootstrap/functions"
+
+module_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1; pwd)"
+echo "$module_path"
+exit 0
+
+
+
 if ! [ -f "$DOTFILES_SCRATCH/gitconfig.dynamic.symlink" ]; then
   source "$DOTFILES_ROOT/bootstrap/functions"
-  git_credential='cache'
+  GIT_CREDENTIAL_HELPER='cache'
   if [ "$(uname -s)" == "Darwin" ]; then
-    git_credential='osxkeychain'
+    GIT_CREDENTIAL_HELPER='osxkeychain'
   fi
 
   user ' - What is your github author name?'
-  read -re git_authorname
+  read -re AUTHORNAME
   user ' - What is your github author email?'
-  read -re git_authoremail
+  read -re AUTHOREMAIL
 
-  sed \
-    -e "s/#AUTHORNAME#/$git_authorname/g" \
-    -e "s/#AUTHOREMAIL#/$git_authoremail/g" \
-    -e "s/#GIT_CREDENTIAL_HELPER#/$git_credential/g" \
-    -e "s!#DOTFILES_ROOT#!$DOTFILES_ROOT!g" \
-    "$DOTFILES_ROOT/git/gitconfig.dynamic.symlink.template" > "$DOTFILES_SCRATCH/gitconfig.dynamic.symlink"
+  render_templates
+
+  #sed \
+  #  -e "s/#AUTHORNAME#/$git_authorname/g" \
+  #  -e "s/#AUTHOREMAIL#/$git_authoremail/g" \
+  #  -e "s/#GIT_CREDENTIAL_HELPER#/$git_credential/g" \
+  #  -e "s!#DOTFILES_ROOT#!$DOTFILES_ROOT!g" \
+  #  "$DOTFILES_ROOT/git/gitconfig.dynamic.symlink.template" > "$DOTFILES_SCRATCH/gitconfig.dynamic.symlink"
 
   log_success 'gitconfig'
 fi
