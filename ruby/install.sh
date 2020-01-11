35624c798d4cd3ababdec30ef0ee515c08d1032a
Jason Scholl <jason.scholl@gemini.com>
2020-01-11 06:46:30 -0800

    clean up ruby install and make it more universal

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
+ruby_versions=("2.5.5")
+
 source "${DOTFILES_ROOT}/bootstrap/functions"
 
-if ! command -v rbenv; then
-  echo "  Installing rbenv for you."
-  brew install rbenv > /tmp/rbenv-install.log
+if ! command -v rbenv > /dev/null; then
+  log_info "Installing rbenv"
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv > /tmp/rbenv-install.log 2>&1
+  # shellcheck disable=SC2015
+  cd ~/.rbenv && src/configure && make -C src >> /tmp/rbenv-install.log || true
 fi
 
-if ! command -v ruby-build; then
-  echo "  Installing ruby-build for you."
-  brew install ruby-build > /tmp/ruby-build-install.log
+if ! rbenv install -l > /dev/null 2>&1; then
+  log_info "Installing ruby-build"
+  git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build >> /tmp/rbenv-install.log 2>&1
 fi
 
-for version in 2.5.5; do
-  if rbenv versions | grep "^\s*$version\s*$"; then
-    log_success "skipped rbenv ruby $version, already installed"
-    continue
-  fi
-
-  rbenv install $version
-
-  if [ $? == 0 ]; then
-    log_success "installed rbenv ruby $version"
+for version in "${ruby_versions[@]}"; do
+  log_info "Installing ruby $version"
+  if rbenv versions | grep -q " *$version *"; then
+    log_success "  already installed"
+  elif rbenv install "$version" > "/tmp/rbenv-install-$version.log" 2>&1; then
+    log_success "  installed ruby $version"
   else
-    log_fail "failed to install rbenv ruby $version"
+    log_fail "  failed to install ruby $version"
   fi
 done
 
-echo 2.5.5 > ~/.rbenv/version
+echo "${ruby_versions[-1]}" > ~/.rbenv/version
 
 rbenv rehash
