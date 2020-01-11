97843238cb5c69442ed7d6cab4c1cc87096115ac
Jason Scholl <jason.scholl@gemini.com>
2020-01-11 08:07:07 -0800

    Improve messaging

--- linux/install.sh
+++ linux/install.sh
@@ -5,17 +5,26 @@ if [ "$(uname -s)" != "Linux" ]; then
   exit 0
 fi
 
-sudo apt update
+source "$DOTFILES_ROOT/bootstrap/functions"
+log_info 'updating apt'
+
+sudo apt update > /dev/null 2>&1
 
 # Install packages
-
-# autocomplete for many tools
-
-# FIXME: install ruby-build (and rbenv?) from git
-
-packages="awscli grc ipcalc tcpint dhcping csshx gnu-tar wget nmap rbenv ruby-build mtr httpie exercism jq ag autojump zsh xsel silversearcher-ag"
+log_info 'installing packages'
+packages="awscli grc ipcalc dhcping wget nmap mtr httpie jq autojump zsh xsel silversearcher-ag"
 for pkg in $packages; do
-  sudo apt install $pkg
+  if dpkg -s "$pkg" > /dev/null 2>&1; then
+    log_success "  $pkg already installed"
+    continue
+  fi
+
+  # shellcheck disable=SC2024
+  if sudo apt install "$pkg" > "/tmp/install_$pkg.log" 2>&1; then
+    log_success "installed $pkg"
+  else
+    log_fail "unable to install $pkg"
+  fi
 done
 
 exit 0
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 source "$DOTFILES_ROOT/bootstrap/functions"
 
-if [ "$SHELL" == "$(command -v zsh)" ]; then
+if [ "$SHELL" -ef "$(command -v zsh)" ]; then
   log_success "zsh is already set as shell"
 else
+  log_info "Changing shell to zsh, please enter password"
   if chsh -s "$(command -v zsh)"; then
-    log_success "Changed shell to zsh"
+    log_success "  Changed shell to zsh"
   else
-    log_failure "Unable to set shell to zsh"
+    log_failure "  Unable to set shell to zsh"
   fi
 fi
