968ce5443681069deca47dc740c64402bddd52ae
Jason Scholl <jason.scholl@gemini.com>
2020-02-01 12:53:43 -0800

    Add more bash completions

--- bash/completions.sh
+++ bash/completions.sh
@@ -1,2 +1,13 @@
-complete -C $DOTFILES_SCRATCH/bin/consul consul
-complete -C $DOTFILES_SCRATCH/bin/vault vault
+# shellcheck disable=SC2148
+aws_completer_path="$(command -v aws_completer)"
+if [ -x "$aws_completer_path" ]; then
+  complete -C "$aws_completer_path" aws
+fi
+
+self_completing_tools=(consul vault terraform)
+for tool_name in "${self_completing_tools[@]}"; do
+  tool_path="$(command -v "$tool_name")"
+  if [ -x "$tool_path" ]; then
+    complete -C "$tool_path" "$tool_name"
+  fi
+done
