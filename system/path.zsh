1b3195fc3ded594a13ef99919ee5794f879b2188
Jason Scholl <jason.scholl@gemini.com>
2020-02-01 12:59:03 -0800

    clean up env vars and PATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,2 @@
 export PATH="$HOME/bin:$DOTFILES_ROOT/bin:${DOTFILES_SCRATCH}/bin:$HOME/.rbenv/bin:/usr/local/sbin:$PATH" 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
