e30faf2799cafdd5f0112b5d428ee2ee13272a95
Jason Scholl <jason.scholl@gemini.com>
2020-02-01 13:40:14 -0800

    fix typo and handle weird zsh paths

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source "$DOTFILES_ROOT/bootstrap/functions"
 
-if [ "$SHELL" -ef "$(command -v zsh)" ]; then
+if [ "$SHELL" -ef "$(command -v zsh)" ] || [ "$SHELL" -ef "$(< /etc/shells grep '/zsh$')" ]; then
   log_success "zsh is already set as shell"
 else
   log_info "Changing shell to zsh, please enter password"
-  if chsh -s "$(command -v zsh)"; then
+  if chsh -s "$(< /etc/shells grep -m 1 '/zsh$')"; then
     log_success "  Changed shell to zsh"
   else
-    log_failure "  Unable to set shell to zsh"
+    log_fail "  Unable to set shell to zsh"
   fi
 fi
