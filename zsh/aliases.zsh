49679b51c487e999aa6de198da130e14cd446d14
jescholl <jason.scholl@meettally.com>
2022-07-06 14:37:27 -0700

    use exa and bat instead of ls and cat

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,5 +5,10 @@ alias be='bundle exec'
 alias dc="docker-compose"
 alias knife="be knife"
 alias wx="curl wttr.in/Portland"
+alias ls="exa"
+alias cat="bat"
+
+alias pcp="pwd; pwd | pbcopy"
+
 
 #alias ansible="prun ansible"
