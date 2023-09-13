5210f89a37961d9d6f12ef8a0d2a964cd66c5ac5
Jason Scholl <jason.e.scholl@gmail.com>
2023-09-13 09:19:21 -0700

    Fix install URL

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,7 +16,7 @@ source "$DOTFILES_ROOT/bootstrap/functions"
 # Check for Homebrew
 if ! command -v brew > /dev/null; then
   log_info "  Installing Homebrew for you."
-  if ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/install-homebrew.log 2>&1; then
+  if ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install)" > /tmp/install-homebrew.log 2>&1; then
     log_fail "Unable to install homebrew"
   fi
 fi
