c13c4bfa798efa974bb5c514555f742887ce9a12
Jason Scholl <jason.e.scholl@gmail.com>
2024-03-11 15:45:26 -0700

    Switch frome exa to eza

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,7 @@ alias be='bundle exec'
 alias dc="docker-compose"
 alias knife="be knife"
 alias wx="curl wttr.in/Portland"
-alias ls="exa"
+alias ls="eza"
 alias cat="bat"
 alias cdm="cd \$(hcl2json terragrunt.hcl | jq -r '.terraform[].source')"
 
