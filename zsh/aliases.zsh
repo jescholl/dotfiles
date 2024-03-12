ef1152647a6b315d6f394d448f41a4a75b0ae81f
jescholl <jason.scholl@meettally.com>
2024-03-11 17:54:31 -0700

    Add assume_role script

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 alias reload!='. ~/.zshrc'
 
 #alias prun='pipenv run'
@@ -13,3 +15,43 @@ alias pcp="pwd; pwd | pbcopy"
 
 
 #alias ansible="prun ansible"
+#
+#
+
+function aws_assume_role() {
+  local args=()
+  local role_arn=""
+
+  while [[ $# -gt 0 ]]; do
+    case $1 in
+      -*)
+        args+=("$1" "$2")
+        shift; shift
+        ;;
+      *)
+        if [[ "$role_arn" == "" ]]; then
+          role_arn="$1"
+          shift;
+        else
+          echo "Invalid arguments"
+          echo $@
+          return 1
+        fi
+        ;;
+    esac
+  done
+
+  aws_cmd=("aws" "sts" "assume-role" "--role-arn" "$role_arn" "--role-session-name" "aws_assume_role_function")
+  aws_cmd+=(${args[@]})
+
+  if assume_role_output=$($aws_cmd); then
+    eval $(echo $assume_role_output | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=" + .AccessKeyId + "\nexport AWS_SECRET_ACCESS_KEY=" + .SecretAccessKey + "\nexport AWS_SESSION_TOKEN=" + .SessionToken')
+
+    echo "Success: role assumed"
+  else
+    echo "ERROR"
+    echo "CMD: $aws_cmd"
+    echo "OUTPUT: $assume_role_output"
+  fi
+
+}
