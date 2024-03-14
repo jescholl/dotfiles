316eb0214458bee846322ed1a0765e85f9fd20b4
Jason Scholl <jason.e.scholl@gmail.com>
2024-03-14 07:25:58 -0700

    Make asdf work better with golang

--- asdf/environment.zsh
+++ asdf/environment.zsh
@@ -1,1 +1,3 @@
 source $DOTFILES_SCRATCH/asdf/tools/asdf/asdf.sh
+
+export ASDF_GOLANG_MOD_VERSION_ENABLED=true
---
+++ asdf/path.zsh
@@ -0,0 +1,4 @@
+. ~/.asdf/plugins/golang/set-env.zsh
+
+export GOBIN="$HOME/go_bin"
+export PATH="$PATH:$GOBIN"
