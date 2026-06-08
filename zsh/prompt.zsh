2b49d981e000f1fdfce94dd8aca74f109b7d4059
Jason Scholl <jason.e.scholl@gmail.com>
2026-06-08 10:46:44 -0700

    remove dead rb_prompt() function

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,14 +2,6 @@ autoload colors && colors
 # cheers, @ehrenmurdick
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-rb_prompt() {
-  ruby_version=$(rbenv version | awk '{print $1}')
-  if ! [[ -z "$ruby_version" ]]; then
-    echo "%{$fg_bold[yellow]%}$ruby_version%{$reset_color%} "
-  else
-    echo ""
-  fi
-}
 
 directory_name() {
   echo "%{$fg[blue]%}%3~%{$reset_color%}"
