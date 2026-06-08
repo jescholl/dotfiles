5f927179f47cdca285aafac71b919399a06c924a
Jason Scholl <jason.e.scholl@gmail.com>
2026-06-08 08:58:41 -0700

    make asdf plugin installs idempotent via add_plugin helper

--- asdf/install.sh
+++ asdf/install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 if command -v asdf; then
-  asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
-  asdf plugin add terragrunt https://github.com/ohmer/asdf-terragrunt.git
-  asdf plugin add nomad https://github.com/asdf-community/asdf-hashicorp.git
-  asdf plugin add vault https://github.com/asdf-community/asdf-hashicorp.git
-  asdf plugin add consul https://github.com/asdf-community/asdf-hashicorp.git
-  asdf plugin add python https://github.com/danhper/asdf-python.git
-  asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
-  asdf plugin add pre-commit https://github.com/jonathanmorley/asdf-pre-commit.git
-  asdf plugin add tfsec https://github.com/woneill/asdf-tfsec.git
-  asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
-  asdf plugin add argocd https://github.com/beardix/asdf-argocd.git
-  asdf plugin add sops https://github.com/feniix/asdf-sops.git
+  add_plugin() {
+    asdf plugin list | grep -q "^$1$" || asdf plugin add "$1" "$2"
+  }
+
+  add_plugin terraform https://github.com/asdf-community/asdf-hashicorp.git
+  add_plugin terragrunt https://github.com/ohmer/asdf-terragrunt.git
+  add_plugin nomad https://github.com/asdf-community/asdf-hashicorp.git
+  add_plugin vault https://github.com/asdf-community/asdf-hashicorp.git
+  add_plugin consul https://github.com/asdf-community/asdf-hashicorp.git
+  add_plugin python https://github.com/danhper/asdf-python.git
+  add_plugin ruby https://github.com/asdf-vm/asdf-ruby.git
+  add_plugin pre-commit https://github.com/jonathanmorley/asdf-pre-commit.git
+  add_plugin tfsec https://github.com/woneill/asdf-tfsec.git
+  add_plugin golang https://github.com/asdf-community/asdf-golang.git
+  add_plugin argocd https://github.com/beardix/asdf-argocd.git
+  add_plugin sops https://github.com/feniix/asdf-sops.git
 
   asdf install
   asdf reshim
