f5b84017a66e6c98594d4127280fcd05039250a7
Jason Scholl <jason.e.scholl@gmail.com>
2026-06-08 10:35:59 -0700

    remove hardcoded Intel Homebrew path, always use brew --prefix

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,8 +1,6 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )); then
-  if [ -f "/usr/local/etc/grc.zsh" ]; then
-    source "/usr/local/etc/grc.zsh"
-  elif (( $+commands[brew] )); then
+  if (( $+commands[brew] )); then
     source "$(brew --prefix)/etc/grc.zsh"
   else
     echo "Unable to find grc.zsh"
