89efa690c0372f86d69ce715c164d3d90b329854
Jason Scholl <jason.e.scholl@gmail.com>
2026-06-18 19:38:40 -0700

    asdf: stop exporting GOROOT so per-dir Go versions resolve correctly

--- asdf/path.zsh
+++ asdf/path.zsh
@@ -1,4 +1,4 @@
-. ~/.asdf/plugins/golang/set-env.zsh
-
+# Intentionally NOT sourcing asdf-golang's set-env.zsh: its GOROOT export freezes the
+# Go version in non-interactive shells. Unset GOROOT = each `go` self-resolves per dir.
 export GOBIN="$HOME/go_bin"
 export PATH="$PATH:$GOBIN"
